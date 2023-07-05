6b7f0f34f1f2eaec379c34ea9b4505d56ea61ffe
khaled <khaedmo@gmail.com>
2023-07-05 12:20:56 +0300

    fix import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uipractice/ui/loginScreen.dart';
+import 'package:uipractice/ui/login/loginScreen.dart';
 
 void main() {
   runApp(const MyApp());
